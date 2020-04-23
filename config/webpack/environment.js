47423169162883114506a39b22cf5641e1b9008b
Kyle Anderson <kra.aue@gmail.com>
2020-04-23 08:49:00 -0400

    Squashed commit of the following:
    commit 993b2e92804d41ca5c71850d03554f082c9b3387
    Author: Kyle Anderson <kra.aue@gmail.com>
    Date:   Thu Apr 23 08:47:47 2020 -0400
    
        Add space to application.scss
    
    commit 3adde5939913c296ad5c11a03521ae1cc05d4a01
    Author: Kyle Anderson <kra.aue@gmail.com>
    Date:   Thu Apr 23 00:18:12 2020 -0400
    
        Remove stylesheet_link_tag
    
    commit f0a58c8cc5df5aea5f6ff2038ab86d6417c8ee76
    Author: Kyle Anderson <kra.aue@gmail.com>
    Date:   Thu Apr 23 00:13:59 2020 -0400
    
        Try again again
    
    commit 97ac107ed45ce936cdb506d530cfb88166e21009
    Author: Kyle Anderson <kra.aue@gmail.com>
    Date:   Thu Apr 23 00:09:37 2020 -0400
    
        Try again
    
    commit 736f86651a2eb6c0b336ae28fdd27c9283f72fa7
    Author: Kyle Anderson <kra.aue@gmail.com>
    Date:   Thu Apr 23 00:03:26 2020 -0400
    
        Try different import order
    
    commit 713556f14ae966962434d13d7607e4d9262a6418
    Author: Kyle Anderson <kra.aue@gmail.com>
    Date:   Wed Apr 22 23:55:23 2020 -0400
    
        Configure environment with webpacker
    
    commit dbc77b61f87925ec51d96ae199f8ef19e77ea556
    Author: Kyle Anderson <kra.aue@gmail.com>
    Date:   Wed Apr 22 23:50:00 2020 -0400
    
        Rework bootstrap import
    
        It became apparent to me that bootstrap is a little funny with webpack, so I've reworked
        things a little to see if it behaves better this way in production.
    
    commit 8adb1d9acb95460e9e2a7b4c3f3a49668a343d26
    Author: Kyle Anderson <kra.aue@gmail.com>
    Date:   Wed Apr 22 23:49:42 2020 -0400
    
        Update yarn packages

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,11 @@
-const { environment } = require('@rails/webpacker')
-const typescript =  require('./loaders/typescript')
+const { environment } = require('@rails/webpacker');
+const typescript = require('./loaders/typescript');
+const webpack = require('webpack');
 
-environment.loaders.prepend('typescript', typescript)
-module.exports = environment
+environment.plugins.append(
+    'Provide',
+    new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery', Popper: ['popper.js', 'default'] }),
+);
+
+environment.loaders.prepend('typescript', typescript);
+module.exports = environment;
