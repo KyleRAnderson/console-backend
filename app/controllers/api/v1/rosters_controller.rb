42d42b7d53a92e4cba7957a0f6615d44430387b5
Kyle Anderson <kra.aue@gmail.com>
2020-05-02 22:24:51 -0400

    Fix rosters controller, add tests
    The rosters controller actually had glaring issues in it that were caught
    while trying to setup tests for it.
    These issues have been resolved I believe, and the tests that exposed them have been created.
    
    The tests are as of yet incomplete, since they lack a bit of detailed checks.

--- app/controllers/api/v1/rosters_controller.rb
+++ app/controllers/api/v1/rosters_controller.rb
@@ -1,21 +1,21 @@
 class Api::V1::RostersController < ApplicationController
   before_action :authenticate_user!
-  before_action :correct_user, except: [:index]
+  before_action :correct_user, except: %i[index create]
 
   def create
-    roster = Roster.create(roster_params)
-    if roster
-      render json: roster
+    roster = current_user.rosters.build(roster_params)
+    if roster.save
+      render json: roster, status: :created
     else
-      render json: roster.errors
+      render json: roster.errors, status: :internal_server_error
     end
   end
 
   def show
-    if self.roster
-      render json: self.roster
+    if roster
+      render json: roster, status: :ok
     else
-      render json: self.roster.errors
+      render json: roster.errors, status: :not_found
     end
   end
 
@@ -25,12 +25,10 @@ class Api::V1::RostersController < ApplicationController
   end
 
   def destroy
-    begin
-      self.roster&.destroy!
-      render status: :ok
-    rescue AciveRecord::RecordNotDestroyed => error
-      render status: :internal_server_error, json: error.record.errors
-    end
+    roster&.destroy!
+    render json: roster, status: :ok
+  rescue ActiveRecord::RecordNotDestroyed => e
+    render status: :internal_server_error, json: e.record.errors
   end
 
   private
@@ -45,6 +43,6 @@ class Api::V1::RostersController < ApplicationController
 
   def correct_user
     @roster = current_user.rosters.find_by(id: params[:id])
-    render json: {}, status: :not_found unless @roster
+    render json: { message: 'Resource not found under this user.' }, status: :not_found unless @roster
   end
 end
--- spec/requests/api/v1/rosters_request_spec.rb
+++ spec/requests/api/v1/rosters_request_spec.rb
@@ -1,33 +1,75 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Rosters", type: :request do
+RSpec.describe 'Api::V1::Rosters', type: :request do
+  describe 'with logged in user' do
+    fixtures :users, :rosters
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/api/v1/rosters/create"
-      expect(response).to have_http_status(:success)
+    before(:each) do
+      @steve = users(:steve)
+      post '/api/v1/login', params: {
+                              user: {
+                                email: @steve.email,
+                                password: '321Passwd$$$',
+                              },
+                            }
+      token = response.headers['Authorization']
+      @headers = { 'Authorization': token }
+    end
+
+    describe 'POST /api/v1/rosters' do
+      describe 'with no participant properties' do
+        it 'creates a new roster, with correct properties' do
+          post '/api/v1/rosters', headers: @headers,
+                                  params: { roster: { name: 'Test roster 1' } }
+          expect(response).to have_http_status(:created)
+          roster = JSON.parse(response.body)
+          expect(roster['id']).not_to be_empty
+          expect(roster['name']).to eq('Test roster 1')
+          expect(roster['user_id']).to eq(@steve.id)
+          expect(roster['participant_properties']).to be_empty
+        end
+      end
+
+      describe 'with participant properties' do
+        it 'creates a new roster, with correct properties' do
+          post '/api/v1/rosters', headers: @headers,
+                                  params: { roster: { name: 'Test roster 2',
+                                                     participant_properties: ['one', 'two', 'three'] } }
+          expect(response).to have_http_status(:created)
+
+          roster = JSON.parse(response.body)
+          expect(roster['id']).not_to be_empty
+          expect(roster['name']).to eq('Test roster 2')
+          expect(roster['user_id']).to eq(@steve.id)
+          expect(roster['participant_properties'].count).to eq(3)
+          expect(roster['participant_properties'][0]).to eq('one')
+          expect(roster['participant_properties'][1]).to eq('two')
+          expect(roster['participant_properties'][2]).to eq('three')
+        end
+      end
+    end
+
+    describe 'GET api/v1/rosters/[roster_id]' do
+      it 'loads the roster with the given id successfully' do
+        get "/api/v1/rosters/#{@steve.rosters.first.id}", headers: @headers
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe 'GET /api/v1/rosters' do
+      it 'loads the user\'s rosters' do
+        get '/api/v1/rosters', headers: @headers
+        expect(response).to have_http_status(:success)
+        rosters = JSON.parse(response.body)
+        expect(rosters.count).to eq(1)
+      end
+    end
+
+    describe 'DELETE /destroy' do
+      it 'returns http success' do
+        delete "/api/v1/rosters/#{@steve.rosters.first.id}", headers: @headers
+        expect(response).to have_http_status(:success)
+      end
     end
   end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/api/v1/rosters/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /index" do
-    it "returns http success" do
-      get "/api/v1/rosters/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/api/v1/rosters/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
