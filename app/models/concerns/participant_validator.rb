21e8540d1d27d665af4400eb26778ad2f8b3101c
Kyle Anderson <kra.aue@gmail.com>
2020-05-03 14:12:48 -0400

    Fix participants validator
    The participants validator had some issues with detecting
    missing properties versus duplicate ones. It's been fixed,
    though more could be done in the future to make its messaging
    more clear.

--- app/models/concerns/participant_validator.rb
+++ app/models/concerns/participant_validator.rb
@@ -1,16 +1,16 @@
 class ParticipantValidator < ActiveModel::Validator
-    def validate(participant)
-        return unless participant.roster
-        participant.roster.participant_properties.each do |key|
-            unless participant.participant_attributes.length <= participant.roster.participant_properties.length
-                participant.errors.add :participant_attributes, "Participant has too many properties." 
-            end
-            found = participant.participant_attributes.select { |attribute| attribute.key == key }
-            if !found
-                participant.errors.add :participant_attributes, "Participant is missing property for #{key}."
-            elsif found.length != 1
-                participant.errors.add :participant_attributes, "Participant has multiple definitions for #{key}"
-            end
-        end
+  def validate(participant)
+    return unless participant.roster
+    participant.roster.participant_properties.each do |key|
+      unless participant.participant_attributes.length <= participant.roster.participant_properties.length
+        participant.errors.add :participant_attributes, 'Participant has too many properties.'
+      end
+      found = participant.participant_attributes.select { |attribute| attribute.key == key }
+      if found.empty?
+        participant.errors.add :participant_attributes, "Participant is missing property for #{key}"
+      elsif found.length > 1
+        participant.errors.add :participant_attributes, "Participant has multiple definitions for #{key}"
+      end
     end
+  end
 end
