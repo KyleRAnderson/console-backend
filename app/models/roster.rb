21be45a68e89141a62df245110c21c013fd84c3a
Kyle Anderson <kra.aue@gmail.com>
2020-05-04 19:35:01 -0400

    Autoformat roster.rb

--- app/models/roster.rb
+++ app/models/roster.rb
@@ -1,13 +1,13 @@
 class Roster < ApplicationRecord
-    belongs_to :user, dependent: :destroy
-    has_many :participants
-    serialize :participant_properties, Array
+  belongs_to :user, dependent: :destroy
+  has_many :participants
+  serialize :participant_properties, Array
 
-    validate :validate_unique_properties
+  validate :validate_unique_properties
 
-    def validate_unique_properties
-        unless self.participant_properties.uniq.length == self.participant_properties.length
-            errors.add :participant_properties, 'Participant properties has duplicate property names'
-        end
+  def validate_unique_properties
+    unless self.participant_properties.uniq.length == self.participant_properties.length
+      errors.add :participant_properties, 'Participant properties has duplicate property names'
     end
+  end
 end
