fed4a4d7a5ad72a49829c7fde65213d4a1c8c767
Kyle Anderson <kra.aue@gmail.com>
2020-05-07 20:45:21 -0400

    Add factory items
    Added some factories for basic items, to be used in testing.

---
+++ spec/factories/factory.rb
@@ -0,0 +1,48 @@
+DEFAULT_PASSWORD = '321Passwd$$$'.freeze
+
+FactoryBot.define do
+  factory :user do
+    transient do
+      num_rosters { rand(0..10) }
+    end
+
+    email { Faker::Internet.email }
+    password { DEFAULT_PASSWORD }
+    confirmed_at { DateTime.now }
+
+    after(:create) do |user, evaluator|
+      create_list(:roster,
+                  evaluator.num_rosters,
+                  user: user)
+    end
+  end
+
+  sequence(:roster_count) { |n| n }
+
+  factory :roster do
+    transient do
+      num_participants { rand(0..10) }
+      num_participant_properties { rand(0..5) }
+    end
+
+    name { "#{user.email}-roster#{generate(:roster_count)}" }
+    participant_properties { num_participant_properties.times.collect { Faker::Lorem.word } }
+    user
+
+    after(:create) do |roster, evaluator|
+      create_list(:participant, evaluator.num_participants, roster: roster)
+    end
+  end
+
+  factory :participant do
+    first { Faker::Name.first_name }
+    last { Faker::Name.last_name }
+    roster
+
+    extras do
+      roster.participant_properties.to_h do |property|
+        [property, Faker::Lorem.word]
+      end
+    end
+  end
+end
