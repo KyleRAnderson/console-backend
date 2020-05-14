fbcc4998d83910f9ea68b528b8775216d32c17d8
Kyle Anderson <kra.aue@gmail.com>
2020-05-14 07:57:14 -0400

    Add changed participant_properties validation to roster model
    Added a validation which checks whether or not the participant_properties for a roster
    has changed in a case where this shouldn't be allowed.
    When the roster has participants, it should be blocked from being changed (until possible better behaviour is implemented).

--- app/models/roster.rb
+++ app/models/roster.rb
@@ -5,6 +5,7 @@ class Roster < ApplicationRecord
 
   validate :validate_unique_properties
   validate :validate_nonempty_properties
+  validate :validate_unchanged_participant_properties, on: :update
 
   def validate_unique_properties
     unless self.participant_properties.uniq.length == self.participant_properties.length
@@ -17,6 +18,12 @@ class Roster < ApplicationRecord
       if property.empty?
         errors.add :roster, 'Roster has empty string participant properties'
       end
+    end
+  end
+
+  def validate_unchanged_participant_properties
+    if participant_properties_changed? && participants.count.positive?
+      errors.add :roster, 'Roster\'s participant properties cannot be changed after participants have been added.'
     end
   end
 end
