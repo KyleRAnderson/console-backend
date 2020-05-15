54939d126754a8473aac7163a6c60e75411c30be
Kyle Anderson <kra.aue@gmail.com>
2020-05-14 20:03:05 -0400

    Remove dependence on destroy from hunt to licenses
    Licenses shouldn't be destroyed when the hunt is destroyed, but instead when their corresponding
    participant is destroyed (which happens when the hunt is destroyed anyway).

--- app/models/hunt.rb
+++ app/models/hunt.rb
@@ -2,7 +2,7 @@ class Hunt < ApplicationRecord
   validates :name, presence: true
 
   belongs_to :roster
-  has_many :licenses, dependent: :destroy
+  has_many :licenses
   has_many :rounds, dependent: :destroy
   has_many :participants, through: :licenses
 end
