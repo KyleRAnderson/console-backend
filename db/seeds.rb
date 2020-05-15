b4bc5af05b47b4fba399c9b242d28379e93b01ae
Kyle Anderson <kra.aue@gmail.com>
2020-05-15 07:56:02 -0400

    Update seeds with hunts

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,23 @@ require 'faker'
   user.password_confirmation = '321Passwd$$$'
   user.confirmed_at = DateTime.now
   user.save!
-  rand(1..5).times do |j|
+  rand(1..5).times do |j| # Creating rosters
     properties = j.times.collect { Faker::Lorem.word }
-    roster = Roster.create(name: "#{user.email} roster #{j + 1}", user: user,
-                           participant_properties: properties)
-    rand(10..75).times do
+    roster = user.rosters.build(name: "#{user.email} roster #{j + 1}",
+                                participant_properties: properties)
+    roster.save!
+    rand(10..75).times do # Creating participants.
       attributes = roster.participant_properties.to_h do |property|
         [property, Faker::Lorem.word]
       end
       roster.participants.build(first: Faker::Name.first_name,
                                 last: Faker::Name.last_name,
                                 extras: attributes).save!
+    end
+
+    rand(0..4).times do # Creating Hunts
+      hunt = roster.hunts.build(name: Faker::Ancient.titan)
+      hunt.save!
     end
   end
 end
