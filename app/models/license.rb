f459bf6e0be6b43e65108fbc348ba9c058d2de25
Kyle Anderson <kra.aue@gmail.com>
2020-05-16 17:24:05 -0400

    Add more license validations, tests to go with it
    The license model received a lot more validations, and I've created a full test suite for the model.

--- app/models/license.rb
+++ app/models/license.rb
@@ -3,11 +3,30 @@ class License < ApplicationRecord
   belongs_to :participant
   has_and_belongs_to_many :matches
 
-  validate :validate_one_license_per_participant_per_hunt
+  validate :validate_one_license_per_participant_per_hunt, on: :create
+  validate :validate_participant_in_roster
+  validate :validate_only_changed_eliminated, on: :update
 
   def validate_one_license_per_participant_per_hunt
     if hunt && participant&.licenses&.find_by(hunt: hunt)
       errors.add(:license, 'Only one license may exist per participant per hunt.')
     end
+  end
+
+  # Ensures that the participant to which this license is assigned is in the roster to which the hunt belongs
+  def validate_participant_in_roster
+    if hunt && participant&.roster != hunt.roster
+      errors.add(:license, 'The participant for this license must be in the same roster to which the hunt belongs.')
+    end
+  end
+
+  def validate_only_changed_eliminated
+    if (changed.select { |attribute| attribute != 'eliminated' }).length > 0
+      errors.add(:license, 'Can only change \'eliminated\' property on licenses.')
+    end
+  end
+
+  def as_json(options = {})
+    super.as_json(options).merge(participant.as_json)
   end
 end
---
+++ spec/models/license_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+def cannot_save_and_errors(resource)
+  expect(resource.save).to be false
+  expect(resource.errors).not_to be_empty
+end
+
+RSpec.describe :license, type: :model do
+  let(:user) { create(:user, num_rosters: 2) }
+  let(:roster) { user.rosters.first }
+  let(:participant) { roster.participants.first }
+  let(:hunt) { roster.hunts.first }
+  let(:participant_wrong_hunt) { user.rosters.second.participants.first }
+
+  subject(:license) do
+    build(:license,
+          participant: roster.participants.first,
+          hunt: hunt)
+  end
+
+  it 'can be created with valid arguments' do
+    expect(license.save).to be true
+  end
+
+  describe 'with participant in different roster from hunt' do
+    it 'cannot be created' do
+      license.participant = participant_wrong_hunt
+      cannot_save_and_errors(license)
+    end
+  end
+
+  it 'cannot be created without participant' do
+    license.participant = nil
+    cannot_save_and_errors(license)
+  end
+
+  it 'cannot be created without hunt' do
+    license.hunt = nil
+    cannot_save_and_errors(license)
+  end
+
+  describe 'with a license already existing for the participant' do
+    it 'cannot be saved, and has errors' do
+      create(:license, participant: participant, hunt: hunt)
+      cannot_save_and_errors(license)
+    end
+  end
+
+  describe 'upon updating attributes other than eliminated' do
+    it 'doesn\'t update' do
+      license.save! # Bang because this should work.
+      license.participant = participant_wrong_hunt
+      cannot_save_and_errors(license)
+    end
+  end
+
+  describe 'upon updating eliminated attribute only' do
+    it 'updates successfully' do
+      license.save!
+      license.eliminated = true
+      expect(license.save).to be true
+    end
+  end
+end
