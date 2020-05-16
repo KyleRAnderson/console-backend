04bdce15000591e8a416ad9cfcf2dc0abba34b8f
Kyle Anderson <kra.aue@gmail.com>
2020-05-16 17:30:49 -0400

    Add new licenses and hunts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users, path: '', path_prefix: 'api/v1/', defaults: { format: 'json' },
                      path_names: { sign_in: 'login', sign_out: 'logout', registration: 'signup' },
                      controllers: { sessions: 'api/v1/sessions', registrations: 'api/v1/registrations', passwords: 'api/v1/passwords', confirmations: 'api/v1/confirmations' }
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :create, :show, :destroy], defaults: { format: 'json' }
-      resources :rosters, only: [:index, :create, :show, :destroy], defaults: { format: 'json' } do
-        resources :participants, only: [:index, :create, :show, :destroy, :update], defaults: { format: 'json' }
+      resources :users, only: %i[index create show destroy], defaults: { format: 'json' }
+      resources :rosters, only: %i[index create show destroy], defaults: { format: 'json' } do
+        resources :participants, only: %i[index create show destroy update], defaults: { format: 'json' }
+      end
+      resources :hunts, only: %i[index create show destroy update], defaults: { format: 'json' } do
+        resources :licenses, only: %i[index create show destroy update], defaults: { format: 'json' }
       end
     end
   end
