563a5c0c81c8f665d1fe4aac18cd8fbe0d5eee22
Kyle Anderson <kra.aue@gmail.com>
2020-05-23 00:23:56 -0400

    Add more to matchmake spec
    The spec still isn't as great as I'd like it to be, but I believe it does at least
    do a good sanity check.

--- spec/models/matchmake_spec.rb
+++ spec/models/matchmake_spec.rb
@@ -1,15 +1,47 @@
 require 'rails_helper'
 
-def check_matches(matches, original_num_licenses)
-  expect(matches.count).to eq(original_num_licenses / 2) # Integer division
-  expect(matches.save_all).to be true
+=begin
+Side note: I could have made several of these into their own
+custom matchers, but it would be ugly and have less
+messaging for which aspect failed.
+=end
+
+def check_matches(matchmake,
+                  original_licenses,
+                  within_properties: [],
+                  between_properties: [],
+                  num_within_overflow: 0,
+                  within_overflow_properties: [],
+                  leftover: false,
+                  &between_validator)
+  matches = matchmake.matches
+  check_match_basics(matches, original_licenses.count)
+  check_all_licenses_present(matches, original_licenses)
+
+  unless within_properties.empty?
+    check_valid_within_matches(matches,
+                               num_overflow: num_within_overflow,
+                               overflow_properties: within_overflow_properties)
+  end
+
+  unless between_properties.empty?
+    check_valid_between_matches(matches, between_properties, &between_validator)
+  end
+
+  check_leftover(matchmake, leftover)
 end
 
-def expect_leftover(matchmake, has_leftover)
+def check_match_basics(matches, original_num_licenses)
+  expect(matches.count).to eq(original_num_licenses / 2) # Integer division
+  expect(matches.save_all).to be true
+  matches.each { |match| expect(match.participants.count).to eq(2) }
+end
+
+def check_leftover(matchmake, has_leftover)
   expect(matchmake.leftover.count).to eq(has_leftover ? 1 : 0)
 end
 
-def expect_all_licenses_present(matches, licenses)
+def check_all_licenses_present(matches, licenses)
   license_ids = licenses.map(&:id).uniq
   matches.each do |match|
     # Don't use match.participants.each here because I want to verify 2 licenses per match
@@ -25,16 +57,10 @@ end
 # overflow_properties: The values of the participant extras that
 # are expected to be found in overflow matches
 # leftover: True if there should be a leftover license, false otherwise
-def check_matches_generated_within(matchmake,
-                                   original_licenses,
-                                   within_properties,
-                                   num_overflow: 0,
-                                   overflow_properties: [],
-                                   leftover: false)
-  matches = matchmake.matches
-  check_matches(matches, original_licenses.count)
-  expect_all_licenses_present(matches, original_licenses)
-
+def check_valid_within_matches(matches,
+                               within_properties,
+                               num_overflow: 0,
+                               overflow_properties: [])
   split_matches = matches.group_by do |match|
     equal_properties = true
     within_properties.each do |property|
@@ -57,8 +83,14 @@ def check_matches_generated_within(matchmake,
       end
     end
   end
+end
 
-  expect_leftover(matchmake, leftover)
+def check_valid_between_matches(matches, between_properties, &validator)
+  associations = matches.group_by do |match|
+    generate_property_digest(match, between_properties)
+  end
+  associations = associations.to_h { |value_digest, matches_group| [value_digest, matches_group.length] }
+  validator&.call(associations)
 end
 
 # Generates a key to use to describe a matchup of certain participant properties in a match
@@ -67,7 +99,7 @@ end
 # [['value1A', 'value2A'], ['value1B', 'value2B']]
 # This is supposed to act as a unique identifier for a certain matchup between participants
 # of different property values.
-def generate_match_between_property_hash(match, between_properties)
+def generate_property_digest(match, between_properties)
   match_properties_hash = match.participants.map do |participant|
     property_values = between_properties.map do |property|
       participant.extras[property]
@@ -77,33 +109,14 @@ def generate_match_between_property_hash(match, between_properties)
   match_properties_hash.sort
 end
 
-def check_matches_generated_between(matchmake,
-                                    original_licenses,
-                                    between_properties,
-                                    num_overflow: 0,
-                                    overflow_properties: [],
-                                    leftover: false)
-  matches = matchmake.matches
-  check_matches(matches, original_licenses.count)
-  expect_all_licenses_present(matches, original_licenses)
-  expect_leftover(matchmake, leftover)
-
-  associations = matches.group_by do |match|
-    generate_match_between_property_hash(match, between_properties)
-  end
-end
-
 def generate_participants(roster, number_lists, &block)
   number_lists.times do |i|
-    number, *property_values = block.call(i)
-    participant_extras = roster.participant_properties.each_with_index.to_h do |property, j|
-      [property, property_values[j].to_s]
-    end
+    number, participant_extras = block.call(i)
     create_list(:participant, number, roster: roster, extras: participant_extras)
   end
 end
 
-RSpec.fdescribe Matchmake do
+RSpec.describe Matchmake do
   # Custom RSpec matchers
 
   PROPERTIES = ['num', 'food'].freeze
@@ -118,25 +131,25 @@ RSpec.fdescribe Matchmake do
 
       context 'with four distinct participant groups' do
         it 'generates a correct match set' do
-          generate_participants(roster, 4) { |index| [10, index] }
+          generate_participants(roster, 4) { |index| [10, { participant_properties[0] => index.to_s }] }
 
           matchmake = Matchmake.new(hunt.licenses, within: participant_properties, round_id: hunt.rounds.first.id)
           matches = matchmake.matchmake
 
           expect(matches).to eq(matchmake.matches)
-          check_matches_generated_within(matchmake, hunt.licenses, participant_properties)
+          check_matches(matchmake, hunt.licenses, within_properties: participant_properties)
         end
       end
 
       context 'with licenses that will generate overflow' do
         it 'generates matches within and handles overflow between' do
-          generate_participants(roster, 4) { |index| [11, index] }
+          generate_participants(roster, 4) { |index| [11, { participant_properties[0] => index.to_s }] }
 
           matchmake = Matchmake.new(hunt.licenses, within: participant_properties, round_id: hunt.rounds.first.id)
           matchmake.matchmake
 
-          check_matches_generated_within(matchmake, hunt.licenses, participant_properties,
-                                         num_overflow: 4, overflow_properties: %w[0 1 2 3])
+          check_matches(matchmake, hunt.licenses, within_properties: participant_properties,
+                                                  num_within_overflow: 4, within_overflow_properties: %w[0 1 2 3])
         end
       end
     end
@@ -147,12 +160,15 @@ RSpec.fdescribe Matchmake do
       context 'without overflow' do
         it 'generates matches correctly' do
           faker_items = UniqueCollectionGenerator.generate(5) { Faker::Food.dish }
-          generate_participants(roster, 5) { |index| [16, index, faker_items[index]] }
+          generate_participants(roster, 5) { |index|
+            [16,
+             { participant_properties[0] => index.to_s, participant_properties[1] => faker_items[index] }]
+          }
 
           matchmake = Matchmake.new(hunt.licenses, within: participant_properties, round_id: hunt.rounds.first.id)
           matchmake.matchmake
 
-          check_matches_generated_within(matchmake, hunt.licenses, participant_properties)
+          check_matches(matchmake, hunt.licenses, within_properties: participant_properties)
         end
       end
 
@@ -160,17 +176,21 @@ RSpec.fdescribe Matchmake do
         it 'generates matches correctly' do
           first_properties = 5.times.map(&:to_s)
           second_properties = UniqueCollectionGenerator.generate(5) { Faker::Food.dish }
-          generate_participants(roster, 5) { |index| [17, first_properties[index], second_properties[index]] }
+          generate_participants(roster, 5) do |index|
+            [17,
+             { participant_properties[0] => first_properties[index],
+              participant_properties[1] => second_properties[index] }]
+          end
 
           matchmake = Matchmake.new(hunt.licenses, within: participant_properties, round_id: hunt.rounds.first.id)
           matchmake.matchmake
 
-          check_matches_generated_within(matchmake,
-                                         hunt.licenses,
-                                         participant_properties,
-                                         num_overflow: 4,
-                                         overflow_properties: first_properties + second_properties,
-                                         leftover: true)
+          check_matches(matchmake,
+                        hunt.licenses,
+                        within_properties: participant_properties,
+                        num_within_overflow: 4,
+                        within_overflow_properties: first_properties + second_properties,
+                        leftover: true)
         end
       end
 
@@ -183,17 +203,18 @@ RSpec.fdescribe Matchmake do
 
           generate_participants(roster, 4) do |index|
             # 15 participants for the first two, to have overflow, then an even 16 for the remaining two.
-            [(0..1).include?(index) ? 15 : 16, first_properties[index], second_properties[index]]
+            [(0..1).include?(index) ? 15 : 16,
+             { participant_properties[0] => first_properties[index], participant_properties[1] => second_properties[index] }]
           end
 
           matchmake = Matchmake.new(hunt.licenses, within: participant_properties, round_id: hunt.rounds.first.id)
           matchmake.matchmake
 
-          check_matches_generated_within(matchmake,
-                                         hunt.licenses,
-                                         participant_properties,
-                                         num_overflow: 2,
-                                         overflow_properties: overflow_properties)
+          check_matches(matchmake,
+                        hunt.licenses,
+                        within_properties: participant_properties,
+                        num_within_overflow: 2,
+                        within_overflow_properties: overflow_properties)
         end
       end
     end
@@ -203,23 +224,76 @@ RSpec.fdescribe Matchmake do
     context 'with a single between property' do
       let(:participant_properties) { PROPERTIES[0..0] }
 
-      context 'with no leftover licenses or overflow matches, and even number of groups' do
-        it 'generates matches correctly' do
-          generate_participants(roster, 4) { |index| [15, index.to_s] }
+      context 'with one-to-one matchable groups,' do
+        it 'generates matches with no leftover or overflow' do
+          property_values = (0..3).map(&:to_s)
+          generate_participants(roster, 4) { |index| [15, { participant_properties[0] => property_values[index] }] }
 
           matchmake = Matchmake.new(hunt.licenses, between: participant_properties, round_id: hunt.rounds.first.id)
           matchmake.matchmake
 
-          check_matches_generated_between(matchmake,
-                                          hunt.licenses,
-                                          participant_properties)
+          check_matches(matchmake,
+                        hunt.licenses,
+                        between_properties: participant_properties) do |associations|
+            expect(associations.length).to eq(2)
+            associations.each do |association, match_count|
+              expect(match_count).to eq(15)
+              association.each do |association_value|
+                expect(property_values.count(association_value[0])).to eq(1)
+                associations.delete(association_value[0])
+              end
+            end
+          end
         end
       end
 
-      context 'with leftover licenses and overflow matches' do
-        it 'generates matches correctly' do
+      context 'with an odd number of licenses and unmatched between groups' do
+        it 'generates matches with a leftover license and between overflow' do
+          first_property_values = (0..2).map(&:to_s)
+          generate_participants(roster, 3) { |index| [6 + index, { participant_properties[0] => first_property_values[index].to_s }] }
+
+          matchmake = Matchmake.new(hunt.licenses, between: participant_properties, round_id: hunt.rounds.first.id)
+          matchmake.matchmake
+
+          check_matches(matchmake,
+                        hunt.licenses,
+                        between_properties: participant_properties,
+                        leftover: true) do |associations|
+            expect(associations.length).to eq(3)
+            expect(associations).to satisfy do |a|
+              a == { [%w[0], %w[1]] => 6, [%w[1], %w[2]] => 1, [%w[2], %w[2]] => 3 } ||
+                a == { [%w[0], %w[2]] => 6, [%w[1], %w[2]] => 2, [%w[1], %w[1]] => 2 } ||
+                a == { [%w[1], %w[2]] => 7, [%w[0], %w[2]] => 1, [%w[0], %w[0]] => 2 }
+            end
+          end
         end
       end
+    end
+  end
+
+  describe 'within and between matchmaking' do
+    let(:within_properties) { ['within'] }
+    let(:between_properties) { ['between'] }
+    let(:participant_properties) { within_properties + between_properties }
+
+    it 'works on a basic level' do
+      within_values = UniqueCollectionGenerator.generate(4) { Faker::Device.platform }
+      between_values = UniqueCollectionGenerator.generate(16) { Faker::Hacker.adjective }
+      generate_participants(roster, 16) do |index|
+        [15, { within_properties[0] => within_values[index % 4],
+              between_properties[0] => between_values[index] }]
+      end
+
+      matchmake = Matchmake.new(hunt.licenses,
+                                within: within_properties,
+                                between: between_properties,
+                                round_id: hunt.rounds.first.id)
+      matchmake.matchmake
+
+      check_matches(matchmake,
+                    hunt.licenses,
+                    within_properties: within_properties,
+                    between_properties: between_properties)
     end
   end
 end
