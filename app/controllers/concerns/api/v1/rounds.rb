02619f208b673b32cb24294c8e973e8878b05458
Kyle Anderson <kra.aue@gmail.com>
2020-05-30 17:24:31 -0400

    Delete unused rounds concern

--- app/controllers/concerns/api/v1/rounds.rb
+++
@@ -1,8 +0,0 @@
-module Api::V1::Rounds
-  def current_round
-    @current_round ||= current_hunt&.rounds&.find_by(number: params[:round_number])
-    head :not_found and return unless @current_round
-
-    @current_round
-  end
-end
