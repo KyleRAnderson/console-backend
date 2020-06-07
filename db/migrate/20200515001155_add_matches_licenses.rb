140568621ce8ed3147dac64f9e80e3dcc8b49033
Kyle Anderson <kra.aue@gmail.com>
2020-06-07 18:03:39 -0400

    Fix create_join_table definition
    Didn't realize how much create_join_table does for you automatically, so
    I've now added the column options properly.

--- db/migrate/20200515001155_add_matches_licenses.rb
+++ db/migrate/20200515001155_add_matches_licenses.rb
@@ -15,9 +15,9 @@ class AddMatchesLicenses < ActiveRecord::Migration[6.0]
       t.timestamps
     end
 
-    create_join_table(:licenses, :matches) do |t|
-      t.belongs_to :license, null: false, foreign_key: true, type: :uuid
-      t.belongs_to :match, null: false, foreign_key: true, type: :uuid
+    create_join_table(:licenses, :matches, column_options: { type: :uuid, foreign_key: true, null: false }) do |t|
+      t.index :license_id
+      t.index :match_id
     end
   end
 end
