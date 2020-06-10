b7d792ca66e560fecc18bdb09be2efee5a40b807
Kyle Anderson <kra.aue@gmail.com>
2020-06-09 21:34:54 -0400

    Add unicode validation to user passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :rosters, dependent: :destroy
+
+  validates_format_of :password, with: /\A[A-Za-z0-9. ! @ # $ % ^ & * ( ) _ - + = ]*\z/,
+                                 message: 'must be unicode characters.'
 end
