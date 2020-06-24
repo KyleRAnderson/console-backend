64d6f0922f47f15671c7c9b0957162fdaa93b1a7
Kyle Anderson <kra.aue@gmail.com>
2020-06-23 22:14:07 -0400

    Revert "Remove useless method from hunt"
    This reverts commit 1c2ff4e54b58370aed6a3b8a9cafc94ab0d686c2.
    
    This method was being used after all, in the as_json for hunts. Need it
    back.

--- app/models/hunt.rb
+++ app/models/hunt.rb
@@ -29,4 +29,10 @@ class Hunt < ApplicationRecord
   def current_round
     rounds.order(number: :desc).first
   end
+
+  private
+
+  def num_active_licenses
+    licenses.where(eliminated: false).count
+  end
 end
