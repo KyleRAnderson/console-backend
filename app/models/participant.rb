92251a2d456a6bffedbd6a4ecfa49b24474128ac
Kyle Anderson <kra.aue@gmail.com>
2020-06-28 00:52:45 -0400

    Add import action to participant model
    The participant model will not accept a file from the controller and
    attempt to upload all the contained participants. This has been tested,
    but needs unit testing.

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,4 +1,6 @@
 class Participant < ApplicationRecord
+  require 'csv'
+
   belongs_to :roster
 
   has_many :licenses, dependent: :destroy, before_add: :ensure_license_participant_unset
@@ -8,6 +10,23 @@ class Participant < ApplicationRecord
   validates :first, presence: true
   validates :last, presence: true
   validates_with ParticipantValidator
+
+  singleton_class.class_eval do # Same as class << self
+    def csv_import(file, roster)
+      raise ArgumentError, 'Invalid Extension' unless file.path.match?(/.*\.csv/i)
+
+      participants = []
+      header_converter = ->(header) { header.to_s.downcase.strip }
+      CSV.foreach(file.path, headers: true, header_converters: header_converter) do |row|
+        hash = row.to_h
+        required = hash.slice('first', 'last')
+        # We include an except for roster here to make sure that it doesn't get included.
+        required['extras'] = hash.except('first', 'last', 'roster')
+        participants << Participant.new(roster: roster, **required)
+      end
+      import participants, all_or_none: true
+    end
+  end
 
   private
 
