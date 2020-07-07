9308f0ed26ceafa5ccc063b8f2e79fb97b5dd5d3
Kyle Anderson <kra.aue@gmail.com>
2020-07-06 20:40:09 -0400

    Improve eliminate half licenses job
    Instead of using the active record import stuff, I now just get random
    license IDs and run an update_all on them. Seems a little simpler,
    though I don't actually know how performance compares.

--- app/jobs/eliminate_half_licenses_job.rb
+++ app/jobs/eliminate_half_licenses_job.rb
@@ -5,12 +5,9 @@ class EliminateHalfLicensesJob < ApplicationJob
   queue_as :default
 
   def perform(round)
-    licenses_to_update = round.matches.ongoing.map do |match|
-      license = match.licenses.sample
-      license.eliminated = true
-      license
+    license_ids_to_update = round.matches.ongoing.map do |match|
+      match.licenses.sample.id
     end
-    License.import licenses_to_update,
-                   on_duplicate_key_update: { conflict_target: [:id], columns: [:eliminated] }
+    License.where(id: license_ids_to_update).update_all(eliminated: true)
   end
 end
