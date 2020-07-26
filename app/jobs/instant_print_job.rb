6e8b669703103ea5b95f35ad1de2be0b82f78024
Kyle Anderson <kra.aue@gmail.com>
2020-07-26 12:45:48 -0400

    Use active storage in InstantPrintJob
    The instant print job will now be using actual storage locations for the
    files it generates and the files that it uses.
    
    This has been tested briefly in the rails console, with the template_pdf
    set manually. Going to continue with mechanisms for uploading the
    template PDF with a controller and viewing the license printout.

--- app/jobs/instant_print_job.rb
+++ app/jobs/instant_print_job.rb
@@ -7,25 +7,44 @@ class InstantPrintJob < ApplicationJob
 
   before_enqueue :validate_arguments
 
-  def perform(current_hunt, ordering_params)
+  def perform(current_hunt, ordering_params = nil)
     current_round = current_hunt.current_round
     # The ordering_params array has been checked at this point, otherwise this could be pretty dangerous.
-    order_cli_args = ordering_params.reduce('') { |total_args, pairing| total_args + " --ordering #{pairing[0]}:#{pairing[1]}" }
-    success = system("java -jar #{JAR_LOCATION} #{current_round.id} --template TestForm.pdf --output test.pdf #{order_cli_args}") # TODO arguments
-    puts "success: #{success}" # FIXME remove
+    order_cli_args = ordering_params&.reduce('') do |total_args, pairing|
+      total_args + " --ordering #{pairing[0]}:#{pairing[1]}"
+    end || ''
+    temp_file = Tempfile.new('output.pdf')
+    begin
+      current_hunt.template_pdf.open do |template_pdf|
+        system("java -jar #{JAR_LOCATION} #{current_round.id} --template #{template_pdf.path} " \
+        "--output #{temp_file.path} #{order_cli_args}")
+      end
+      # Will purge the existing one if there is one.
+      current_hunt.license_printout.attach(io: File.open(temp_file.path), filename: 'printout.pdf', content_type: 'application/pdf')
+    ensure
+      temp_file.unlink
+    end
   end
 
   private
 
   def validate_arguments
-    current_round = arguments.first.current_round
+    current_hunt = arguments.first
+    current_round = current_hunt.current_round
     ordering_params = arguments.second
     raise ArgumentError, 'Cannot perform Instant Print without a round in the current hunt' if current_round.blank?
 
-    return unless ordering_params.present?
+    errors = []
 
-    unless ordering_params.is_a?(Array) && ordering_params.all? { |pairing| pairing.is_a?(Array) && pairing.size == 2 }
-      raise ArgumentError, 'Ordering params needs to be an array of tuples of length 2 each.'
+    if ordering_params.present? &&
+       (!ordering_params.is_a?(Array) || !ordering_params.all? { |pairing| pairing.is_a?(Array) && pairing.size == 2 })
+      errors << 'Ordering params needs to be an array of tuples of length 2 each.'
     end
+
+    unless current_hunt.template_pdf.present?
+      errors << 'The given hunt does not have a Template PDF. Please configure one first.'
+    end
+
+    raise ArgumentError, errors.join('\n') if errors.present?
   end
 end
