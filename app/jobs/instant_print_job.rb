d2cf0529aea03d0ea5ea87221230771c2b336453
Kyle Anderson <kra.aue@gmail.com>
2020-07-25 20:41:07 -0400

    Add ordering to InstantPrintJob
    The ordering options that were added in the pin press have made their
    way to being available in the InstantPrint job now.

--- app/jobs/instant_print_job.rb
+++ app/jobs/instant_print_job.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class InstantPrintJob < ApplicationJob
   JAR_LOCATION = 'lib/script/pin-press.jar'
 
@@ -5,9 +7,11 @@ class InstantPrintJob < ApplicationJob
 
   before_enqueue :validate_arguments
 
-  def perform(current_hunt)
+  def perform(current_hunt, ordering_params)
     current_round = current_hunt.current_round
-    success = system("java -jar #{JAR_LOCATION} #{current_round.id}")
+    # The ordering_params array has been checked at this point, otherwise this could be pretty dangerous.
+    order_cli_args = ordering_params.reduce('') { |total_args, pairing| total_args + " --ordering #{pairing[0]}:#{pairing[1]}" }
+    success = system("java -jar #{JAR_LOCATION} #{current_round.id} --template TestForm.pdf --output test.pdf #{order_cli_args}") # TODO arguments
     puts "success: #{success}" # FIXME remove
   end
 
@@ -15,6 +19,13 @@ class InstantPrintJob < ApplicationJob
 
   def validate_arguments
     current_round = arguments.first.current_round
+    ordering_params = arguments.second
     raise ArgumentError, 'Cannot perform Instant Print without a round in the current hunt' if current_round.blank?
+
+    return unless ordering_params.present?
+
+    unless ordering_params.is_a?(Array) && ordering_params.all? { |pairing| pairing.is_a?(Array) && pairing.size == 2 }
+      raise ArgumentError, 'Ordering params needs to be an array of tuples of length 2 each.'
+    end
   end
 end
