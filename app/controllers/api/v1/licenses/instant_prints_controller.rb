377c027b23a164cc35e4694b29844b7b694dc3f9
Kyle Anderson <kra.aue@gmail.com>
2020-07-30 20:52:25 -0400

    Add new instant print options
    The QR code text fill as well as the custom message options are now both
    supported on the rails side.

--- app/controllers/api/v1/licenses/instant_prints_controller.rb
+++ app/controllers/api/v1/licenses/instant_prints_controller.rb
@@ -2,15 +2,15 @@ class Api::V1::Licenses::InstantPrintsController < ApplicationController
   include Api::V1::Hunts
 
   respond_to :json
-  wrap_parameters :print, include: %i[orderings]
+  wrap_parameters :print, include: %i[orderings message]
 
   before_action :authenticate_user!
   before_action :current_hunt
 
   # Used to create a new instant print
-  # Expected body: { print: {orderings: [['<extras property name>', 'asc' | 'desc'>], ...]}}
+  # Expected body: { print: {orderings: [['<extras property name>', 'asc' | 'desc'>], ...], message: 'A custom message'}}
   def create
-    InstantPrintJob.perform_later(current_hunt, params.dig(:print, :orderings))
+    InstantPrintJob.perform_later(current_hunt, params.dig(:print, :orderings), params.dig(:print, :message))
     head :accepted
   rescue ArgumentError => e
     render plain: e, status: :bad_request
--- app/jobs/instant_print_job.rb
+++ app/jobs/instant_print_job.rb
@@ -8,21 +8,29 @@ class InstantPrintJob < ApplicationJob
 
   before_enqueue :validate_arguments
 
-  def perform(current_hunt, ordering_params = nil)
+  def perform(current_hunt, ordering_params = nil, message = nil)
     current_round = current_hunt.current_round
     # The ordering_params array has been checked at this point, otherwise this could be pretty dangerous.
     order_cli_args = ordering_params&.reduce('') do |total_args, pairing|
       total_args + " --ordering #{pairing[0]}:#{pairing[1]}"
     end || ''
+
+    message_arg = message.present? ? "--message '#{message}'" : ''
+    match_view_url = Rails.application.routes.url_helpers.frontend_match_view_url(current_hunt, ':match_id', host: host, port: port)
+    match_view_url[':match_id'] = '%s' # Ready for java's replacement.
+
     temp_file = Tempfile.new('output.pdf')
     begin
       success = false
       current_hunt.template_pdf.open do |template_pdf|
-        success = system("java -jar #{JAR_LOCATION} #{current_round.id} --template #{template_pdf.path} " \
-        "--output #{temp_file.path} #{order_cli_args}")
+        args = "java -jar #{JAR_LOCATION} #{current_round.id} --template #{template_pdf.path} " \
+        "--output #{temp_file.path} #{order_cli_args} #{message_arg} --fill '#{match_view_url}'"
+        success = system(args)
       end
-      # Will purge the existing one if there is one.
-      current_hunt.license_printout.attach(io: File.open(temp_file.path), filename: 'printout.pdf', content_type: 'application/pdf')
+      if success
+        # Will purge the existing one if there is one.
+        current_hunt.license_printout.attach(io: File.open(temp_file.path), filename: 'printout.pdf', content_type: 'application/pdf')
+      end
 
       # Broadcast to action cable.
       broadcast_result(current_hunt, success)
