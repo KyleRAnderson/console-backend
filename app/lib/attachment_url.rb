b16af57b954c4571946d48e7ec6e60361488c837
Kyle Anderson <kra.aue@gmail.com>
2020-07-30 20:50:46 -0400

    Expand functionality of attachment_url
    The attachment_url module is now useful for a little bit more now, as it
    can provide easy access to the host and port that it sneaks via the
    action mailer's configuration.

--- app/lib/attachment_url.rb
+++ app/lib/attachment_url.rb
@@ -1,10 +1,15 @@
 module AttachmentUrl
-  @@host = Rails.application.config.action_mailer.default_url_options[:host]
-  @@port = Rails.application.config.action_mailer.default_url_options[:port]
-
   # Performs the same function as rails_blob_url, but loads the hostname and port information from the mailer config.
   # If attachment is blank, nil is returned.
   def attachment_url(attachment)
-    attachment.blank? ? nil : Rails.application.routes.url_helpers.rails_blob_url(attachment, host: @@host, port: @@port)
+    attachment.blank? ? nil : Rails.application.routes.url_helpers.rails_blob_url(attachment, host: host, port: port)
+  end
+
+  def host
+    Rails.application.config.action_mailer.default_url_options[:host]
+  end
+
+  def port
+    Rails.application.config.action_mailer.default_url_options[:port]
   end
 end
