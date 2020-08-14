5c16f7c46dc094b55a762f33d559ac48d5ab492d
Kyle Anderson <kra.aue@gmail.com>
2020-08-14 19:44:01 -0400

    Use mailjet with regular smtp
    Instead of using the mailjet gem, I'm just going to use plain smtp.
    Seems simpler.

--- Gemfile
+++ Gemfile
@@ -41,9 +41,6 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'will_paginate', '~> 3.1'
 
-# Mail service
-gem 'mailjet', '~> 1.1'
-
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'https://hunt-console.herokuapp.com' }
 
-  config.action_mailer.delivery_method = :mailjet
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.smtp_settings = {
