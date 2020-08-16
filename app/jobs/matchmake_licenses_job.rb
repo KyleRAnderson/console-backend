a2a85f9168c7bf65b29dd4f75171b8eae192de8c
Kyle Anderson <kra.aue@gmail.com>
2020-08-15 23:45:52 -0400

    Fix MatchmakeLicensesJob
    Working towards #52, this fixes the actual issue.
    Remaining steps are to add tests for it.

--- app/jobs/matchmake_licenses_job.rb
+++ app/jobs/matchmake_licenses_job.rb
@@ -5,11 +5,13 @@ class MatchmakeLicensesJob < ApplicationJob
     within = within_between_properties[:within]
     between = within_between_properties[:between]
     throw :no_hunt unless hunt
-    hunt.rounds.create if hunt.rounds.empty?
-    query = hunt.licenses.where(eliminated: false).left_outer_joins(:matches)
-    licenses = query.where(matches: { id: nil }).or(query.where.not(matches: { round_id: hunt.current_round.id }))
-
-    return if licenses.empty?
+    hunt.rounds.create if hunt.rounds.blank?
+    licenses = hunt.licenses.where(eliminated: false)
+      .joins(License.sanitize_sql_array(['LEFT OUTER JOIN licenses_matches ON licenses_matches.license_id = 
+        licenses.id LEFT OUTER JOIN matches ON matches.id = licenses_matches.match_id AND matches.round_id = ?',
+                                         hunt.current_round.id]))
+      .where(matches: { id: nil }).distinct
+    return if licenses.blank?
 
     matchmake = Matchmake.new(licenses.includes(:participant), round_id: hunt.current_round.id, within: within, between: between)
     matches = matchmake.matchmake
