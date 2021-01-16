e54202699602ccba3d8e86555e8a1678d1841004
Kyle Anderson <kra.aue@gmail.com>
2021-01-16 15:37:11 -0500

    Configure RSpec state file
    I've configured the RSpec state file to keep some data between runs of
    RSpec which is useful for invocations like running only the failed
    examples with `rspec --only-failures` as described here:
    https://relishapp.com/rspec/rspec-core/docs/command-line/only-failures.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -55,6 +55,11 @@ RSpec.configure do |config|
   # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
   config.filter_run_when_matching :focus
 
+  # Allows RSpec to persist some state between runs in order to support
+  # the `--only-failures` and `--next-failure` CLI options. We recommend
+  # you configure your source control system to ignore this file.
+  config.example_status_persistence_file_path = 'spec/examples.txt'
+
   # The settings below are suggested to provide a good initial experience
   # with RSpec, but feel free to customize to your heart's content.
 =begin
