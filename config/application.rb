9a525dd4471108f8dc185b9f30007a4a13124986
Kyle Anderson <kra.aue@gmail.com>
2021-01-17 22:23:04 -0500

    Use suggested config for application.rb
    I ran rails app:update:configs, and I declined all suggestions except I
    looked at the diff for application.rb, and took some of what was
    suggested.
    
    I was originally getting an issue with the tests after doing this, but
    this fix worked:
    https://github.com/fgrehm/letter_opener_web/issues/46#issuecomment-273975173.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,21 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'rails'
+# Pick the frameworks you want:
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'action_view/railtie'
+require 'action_cable/engine'
+# See https://github.com/fgrehm/letter_opener_web/issues/46#issuecomment-273975173 for an error that I was getting while this
+# line was commented out.
+require 'sprockets/railtie'
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,7 +24,7 @@ Bundler.require(*Rails.groups)
 module HuntConsole
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
     # API Only application. This should remain at the top
     config.api_only = false # FIXME revert to true one day when the tests start working again
