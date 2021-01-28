20b41f4fc59111e9a27830a3d2e07e5b3f600620
Kyle Anderson <kra.aue@gmail.com>
2021-01-27 20:11:55 -0500

    Skip CSRF protection in certain areas
    Certain actions shouldn't have CSRF protection on them (to my
    understanding) so I've skipped the hook that checks it for those
    actions.

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -2,4 +2,6 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
   include ActionController::MimeResponds
 
   respond_to :json
+
+  skip_forgery_protection only: [:create]
 end
--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::SessionsController < Devise::SessionsController
   include ActionController::MimeResponds
 
+  # Allow users to login, and then afterwards we must prevent CSRF attacks.
+  skip_forgery_protection only: [:create]
+
   respond_to :json
 end
