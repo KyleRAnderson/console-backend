317d07cb00dceb8b14379ea63c6f56f6f616851d
Kyle Anderson <kra.aue@gmail.com>
2021-01-30 09:19:36 -0500

    Test 5

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,6 +4,7 @@
 allowed_host = Rails.application.config.frontend_host_url
 if allowed_host.present?
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    puts "Allowing host: #{allowed_host}" # FIXME REMOVE
     allow do
       origins allowed_host
       resource '*',
