0c109e4f6ed20d3534f52832c5488b9f43fb206c
Kyle Anderson <kra.aue@gmail.com>
2021-01-29 19:34:24 -0500

    Test 2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 # See https://github.com/cyu/rack-cors, https://guides.rubyonrails.org/configuring.html#configuring-middleware
 allowed_host = Rails.application.config.frontend_host_url
 if allowed_host.present?
+  puts 'Setting allowed host' # FIXME remove
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins 'https://hunt-console-frontend.herokuapp.com'
@@ -9,6 +12,6 @@ if allowed_host.present?
                methods: [:get, :post, :patch, :put, :delete, :options, :head],
                credentials: true # Allow secure cookie access
     end
-    Rails.application.config.hosts << 'hunt-console-frontend.herokuapp.com'
   end
+  Rails.application.config.hosts << 'hunt-console-frontend.herokuapp.com'
 end
